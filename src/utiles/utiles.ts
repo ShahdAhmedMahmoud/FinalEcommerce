a63f41cb6df3c61212e5a79f67b50906032c0bf0
ShahdAhmedMahmoud <shahd4563@gmail.com>
2026-06-28 05:01:51 +0300

    fix session token cookie name for production

--- src/utiles/utiles.ts
+++ src/utiles/utiles.ts
@@ -1,12 +1,28 @@
+// import { decode } from "next-auth/jwt";
+// import { cookies } from "next/headers";
+
+// export async function getMyUserToken(){
+  
+//   const cookie= await cookies();
+//   const sessionToken = cookie.get('next-auth.session-token')?.value;
+//   const decodedToken = await decode({token :sessionToken,secret:process.env.NEXTAUTH_SECRET ||""})
+    
+//   console.log('decodedToken',decodedToken);
+//   return decodedToken?.credentialsToken;
+// }
+
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getMyUserToken(){
   
-  const cookie= await cookies();
-  const sessionToken = cookie.get('next-auth.session-token')?.value;
-  const decodedToken = await decode({token :sessionToken,secret:process.env.NEXTAUTH_SECRET ||""})
+  const cookie = await cookies();
+  const sessionToken = 
+    cookie.get('__Secure-next-auth.session-token')?.value ||
+    cookie.get('next-auth.session-token')?.value;
     
-  console.log('decodedToken',decodedToken);
+  const decodedToken = await decode({token: sessionToken, secret: process.env.NEXTAUTH_SECRET || ""})
+    
+  console.log('decodedToken', decodedToken);
   return decodedToken?.credentialsToken;
 }
