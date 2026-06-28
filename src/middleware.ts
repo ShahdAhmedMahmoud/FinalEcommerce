1a335b7fa352d9a7db0596a7ba332aede8b1e488
ShahdAhmedMahmoud <shahd4563@gmail.com>
2026-06-28 06:26:07 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,44 @@
-import { getServerSession } from "next-auth";
-import { getToken } from "next-auth/jwt";
-import {NextRequest , NextResponse} from "next/server";
+// import { getServerSession } from "next-auth";
+// import { getToken } from "next-auth/jwt";
+// import {NextRequest , NextResponse} from "next/server";
 
-export default async function middleware(req:NextRequest){
+// export default async function middleware(req:NextRequest){
    
-  const jwt = await  getToken({req});
-//   console.log("jwt",jwt);
+//   const jwt = await  getToken({req});
+// //   console.log("jwt",jwt);
 
-  if(jwt ){
+//   if(jwt ){
+//     return NextResponse.next();
+//   }
+
+
+
+//     return NextResponse.redirect(`${process.env.MY_DOMAIN}/login`);
+
+// }
+
+// // export const config = {
+// //     // matcher : ['/cart','/payment'],
+// //     matcher : ['/cart:path*'],
+// // };
+// export const config = {
+//     matcher: ['/cart/:path*'],
+// };
+
+
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const jwt = await getToken({ req });
+
+  if (jwt) {
     return NextResponse.next();
   }
 
-
-
-    return NextResponse.redirect(`${process.env.MY_DOMAIN}/login`);
-
+  return NextResponse.redirect(`${process.env.MY_DOMAIN}/login`);
 }
 
-// export const config = {
-//     // matcher : ['/cart','/payment'],
-//     matcher : ['/cart:path*'],
-// };
 export const config = {
-    matcher: ['/cart/:path*'],
+  matcher: ['/cart/:path*'],
 };
